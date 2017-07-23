4cbf325665a59d76a9d903075ab1a38dbfb4c6f2
zce <w@zce.me>
2017-07-23 22:35:50 +0800

    Update markdown preview plugin

--- MarkdownPreview.py
+++ MarkdownPreview.py
@@ -2,13 +2,14 @@ import webbrowser
 
 import sublime_plugin
 
-class MarkdownPreviewCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-        if self.view.file_name():
-            webbrowser.open_new_tab("file://" + self.view.file_name())
+class MarkdownPreviewCommand (sublime_plugin.TextCommand) :
 
-    def is_visible(self):
-        return self.view.file_name() is not None and (
-            self.view.file_name()[-3:] == ".md" or
-            self.view.file_name()[-9:] == ".markdown" or
-            self.view.file_name()[-6:] == ".mdown")
+  def run (self, edit) :
+    self.view.file_name() and webbrowser.open_new_tab('file://' + self.view.file_name())
+
+  def is_visible (self) :
+    return self.view.file_name() is not None and (
+      self.view.file_name()[-3:] == '.md' or
+      self.view.file_name()[-9:] == '.markdown' or
+      self.view.file_name()[-6:] == '.mdown'
+    )
